b182245c308dc22ca688def3fadbfb4f4cb23d3d
Laurent Gautier <lgautier@gmail.com>
2022-10-29 15:03:30 -0400

    Move build/install to PEP 517.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from distutils.core import setup
-
-pack_version = __import__('rpy2_Matrix').__version__
+import setuptools
 
 if __name__ == '__main__':
-    setup(
-        name='rpy2-Matrix',
-        version=pack_version,
-        description='Mapping the R package Matrix',
-        license='GPLv2+',
-        requires=['rpy2'],
-        packages=['rpy2_Matrix'],
-        classifiers=['Programming Language :: Python',
-                     'Programming Language :: Python :: 3',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8',
-                     ('License :: OSI Approved :: GNU General '
-                      'Public License v2 or later (GPLv2+)'),
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Science/Research',
-                     'Development Status :: 5 - Production/Stable']
-    )
+    setuptools.setup()
